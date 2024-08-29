08a8a4c0c790d25d52e8eb40939e7f73b44ae4ed
0xdagarn <sgnoo@naver.com>
2024-08-29 12:51:07 +0900

    add frame metatags

--- index.js
+++ index.js
@@ -5,22 +5,34 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send(`
     <!DOCTYPE html>
-    <html lang="en">
+    <html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>OG Meta Tags Example</title>
+        <title>My Node.js App with Custom Meta Tags</title>
 
-        <!-- Open Graph Meta Tags -->
-        <meta property="og:title" content="My OG Meta Tags Example" />
-        <meta property="og:description" content="This is a simple example of an HTML page with Open Graph meta tags." />
-        <meta property="og:image" content="https://miro.medium.com/v2/resize:fit:1200/1*4KdmDFFwlS1EbQmtqx180g.jpeg" />
-        <meta property="og:url" content="http://localhost:3000/" />
-        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://fc-dev-call.replit.app/image" />
+
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://fc-dev-call.replit.app/image" />
+
+        <meta property="fc:frame:button:1" content="Spec" />
+        <meta property="fc:frame:button:1:action" content="link" />
+        <meta property="fc:frame:button:1:target" content="https://docs.farcaster.xyz/reference/frames/spec#constructing-a-frame" />
+
+        <meta property="fc:frame:button:2" content="Image" />
+        <meta property="fc:frame:button:2:action" content="link" />
+        <meta property="fc:frame:button:2:target" content="https://fc-dev-call.replit.app/image" />
+
+        <meta property="fc:frame:button:3" content="Frame Validator" />
+        <meta property="fc:frame:button:3:action" content="link" />
+        <meta property="fc:frame:button:3:target" content="https://warpcast.com/~developers/frames" />
+
+        <meta property="fc:frame:button:4" content="Replit" />
+        <meta property="fc:frame:button:4:action" content="link" />
+        <meta property="fc:frame:button:4:target" content="https://replit.com/@VarunSrinivasa4/FrailStridentDisc#index.js" />
     </head>
     <body>
         <h1>Welcome to the OG Meta Tags Example</h1>
-        <p>This page includes Open Graph meta tags for better social media integration.</p>
+        <p>This page includes Open Graph and FC Frame meta tags for better social media and FC frame integration.</p>
     </body>
     </html>
   `);
